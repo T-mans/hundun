095c0ad5231ed53858fd5b337919ef957fdb63f6
Wang Nannan <blukis@foxmail.com>
2021-03-03 17:36:03 +0800

    update

---
+++ KotlinKoansDemo/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.useIR = true
+
+plugins {
+    kotlin("jvm") version "1.4.31"
+    application
+}
+
+group = "me.qxy9763"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
